b0b3e3e559ddb1a8f5498a3ac8b0b6f489b6e6f4
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-02-16 14:20:32 +0300

    Initial Commit

---
+++ chessHorse/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chessHorse
+//
+//  Created by Diana Ovechkina on 16.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
